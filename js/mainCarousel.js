feafa1f8ca824134b6b3504884533ff21a7d7226
Yoon Gun-hee <yoongunhee92@gmail.com>
2021-08-08 16:28:59 +0900

    🚧 Import tiny-slider.js to the project and initialize mainCarousel

---
+++ js/mainCarousel.js
@@ -0,0 +1,11 @@
+const mainCarousel = tns({
+  container: '.main-carousel .slider-list',
+  controls: false,
+  navContainer: '.main-carousel .nav-list',
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
